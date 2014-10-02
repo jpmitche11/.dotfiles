50147f5aa8842de0848847c2b636e787109ff63f
Patrick Mitchell <patrick.mitchell@vbrick.com>
2014-10-02 11:43:56 -0400

    Initial commit

---
+++ alias.sh
@@ -0,0 +1,13 @@
+alias pw="pwd"
+
+alias vizrc="vi ~/.zshrc"
+
+alias l="ls"
+alias ll="ls -lhrt"
+alias la="ls -lhArt"
+
+alias gpu='git push -u origin $(current_branch)'
+alias gpp='git push origin $(current_branch)'
+alias gs='git status'
+
+alias gl='git log'
